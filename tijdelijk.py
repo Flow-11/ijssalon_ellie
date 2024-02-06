c3f6175926a6682c3e3b868a6ca5f99830749346
Florus Werff <floruswerff@hotmail.com>
2024-02-06 15:35:08 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,36 @@
+prijzen = {
+    "aarbei": 3,
+    "vanille": 4,
+    "chocolade": 5,
+}
+aanbieding = prijzen["vanille"] * 0.8
+print(aanbieding)
+
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}"
+
+lente_tekst = len(reclame_tekst) + 3
+reclame_tekst2 = reclame_tekst[:lente_tekst]
+print(reclame_tekst2)
+
+# vraag 6
+reclame_tekst3 = reclame_tekst2.upper()
+print(reclame_tekst3)
+
+# vraag 7
+reclame_tekst4 = reclame_tekst3.split(" ")
+print(reclame_tekst4)
+
+
+#vraag 8,9,10
+for el in reclame_tekst4:   
+    lengte = len(el)
+    if lengte > 4:
+        print(el)
+    else :
+        print(el.lower())
+
+
+
+
+
+  
